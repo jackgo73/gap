a8f1c3eb482557f52a94671d700de6fa2c526378
jackgo73 <jackgo73@outlook.com>
2021-05-06 19:56:16 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,40 @@
 
 #include <iostream>
+#include <stdlib.h>
+
+typedef enum ForkNumber {
+    InvalidForkNumber = -1,
+    MAIN_FORKNUM = 0,
+    FSM_FORKNUM,
+    VISIBILITYMAP_FORKNUM,
+    INIT_FORKNUM
+} ForkNumber;
+
+typedef int File;
+typedef int BlockNumber;
+#define MAX_FORKNUM     INIT_FORKNUM
+typedef struct _MdfdVec {
+    File mdfd_vfd;
+    BlockNumber mdfd_segno;
+} MdfdVec;
+
+typedef struct SMgrRelationData {
+    int md_num_open_segs[MAX_FORKNUM + 1];
+    struct _MdfdVec *md_seg_fds[MAX_FORKNUM + 1];
+} SMgrRelationData;
 
 int main() {
     std::cout << "haha" << std::endl;
+
+    MdfdVec *v;
+    SMgrRelationData *reln = (SMgrRelationData *)malloc(sizeof(SMgrRelationData));
+
+    int targetseg = 0;
+    int forknum = 0;
+
+    v = &reln->md_seg_fds[forknum][targetseg];
+
+    v = &reln->md_seg_fds[forknum][reln->md_num_open_segs[forknum] - 1];
+
     return 0;
 }
