872af77b0fc24fb022a99ee652ecb637a2fd353b
jackgo73 <jackgo73@outlook.com>
2021-05-09 20:58:11 +0800

    update

--- src/common.h
+++ src/common.h
@@ -1,12 +1,19 @@
 
 #ifndef ROTATE_COMMON_H
 #define ROTATE_COMMON_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
 struct ListNode {
     int val;
     struct ListNode *next;
+};
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
 };
 
 #endif //ROTATE_COMMON_H
---
+++ src/p_00993_is_cousins/is_cousins.h
@@ -0,0 +1,8 @@
+#ifndef P_00993_IS_COUSINS
+#define P_00993_IS_COUSINS
+
+#include "common.h"
+
+bool isCousins(struct TreeNode *root, int x, int y);
+
+#endif //P_00993_IS_COUSINS
