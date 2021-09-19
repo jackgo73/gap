0d8cef2486bc897d3a4f888789ce00bb1b0ec52f
jackgo73 <jackgo73@outlook.com>
2021-09-19 20:19:36 +0800

    update

--- tst/ut_00014_longest_common_prefix.cpp
+++ tst/ut_00014_longest_common_prefix.cpp
@@ -2,5 +2,23 @@
 #include "p_00014_longest_common_prefix.c"
 
 TEST(LONGEST_COMMON_PREFIX, TEST01) {
-	EXPECT_EQ (1, 1);
+    char **input;
+    input = (char **) malloc(sizeof(char *) * 3);
+    input[0] = strdup("abcdef");
+    input[1] = strdup("abzde");
+    input[2] = strdup("abdef");
+    EXPECT_STRCASEEQ(longestCommonPrefix(input, 3), "ab");
+}
+
+TEST(LONGEST_COMMON_PREFIX, TEST02) {
+    char **input = NULL;
+    EXPECT_STRCASEEQ(longestCommonPrefix(input, 0), "");
+}
+
+TEST(LONGEST_COMMON_PREFIX, TEST03) {
+    char **input;
+    input = (char **) malloc(sizeof(char *) * 2);
+    input[0] = strdup("");
+    input[1] = strdup("");
+    EXPECT_STRCASEEQ(longestCommonPrefix(input, 2), "");
 }
