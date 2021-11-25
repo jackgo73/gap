7b052409a9758bd22b3872af578221a5c083f9e4
jackgo73 <jackgo73@outlook.com>
2021-11-25 23:26:43 +0800

    update

--- src/include/slist.h
+++ src/include/slist.h
@@ -25,8 +25,22 @@ struct ListNode *makeListNode1(const int *arr, int cnt) {
     return startPos;
 }
 
-struct ListNode **makeListNode2(const int arr[][], int row, int col) {
-
+struct ListNode **makeListNode2(int **arr, int row) {
+    struct ListNode **r = (struct ListNode **) malloc(sizeof(struct ListNode *) * row);
+    for (int i = 0; i < row; ++i) {
+        int *curRow = arr[i];
+        int cntRow = curRow[0];
+        r[i] = (struct ListNode *) malloc(SizeOfListNode * cntRow);
+        struct ListNode *p = r[i];
+        for (int j = 0; j < cntRow - 1; ++j) {
+            p->val = curRow[j + 1];
+            p->next = p + 1;
+            p = p->next;
+        }
+        p->val = curRow[cntRow];
+        p->next = NULL;
+    }
+    return r;
 }
 
 
--- tst/include/slist_UT.cpp
+++ tst/include/slist_UT.cpp
@@ -4,18 +4,30 @@
 
 TEST(slist, TEST01) {
     int arr[] = {1, 2, 3, 4, 5};
-    ListNode *list1 = makeListNode1(arr, 5);
-    EXPECT_EQ (1, 1);
+
+    ListNode *l = makeListNode1(arr, 5);
+
+    EXPECT_EQ (l->val, 1);
+    EXPECT_EQ (l->next->val, 2);
+    EXPECT_EQ (l->next->next->val, 3);
 }
 
 TEST(slist, TEST02) {
-    int arr[3][3] = {
-            {1, 4, 5},
-            {1, 3, 4},
-            {2, 6}
-    };
-    ListNode **list2 = makeListNode2(arr, 3, 3);
-    EXPECT_EQ (1, 1);
+    const int rowCnt = 3;
+    int r1[] = {3, 1, 4, 5};
+    int r2[] = {3, 1, 3, 4};
+    int r3[] = {2, 2, 6};
+
+    int *r[rowCnt] = {r1, r2, r3};
+    struct ListNode **l = makeListNode2(r, rowCnt);
+    EXPECT_EQ (l[0]->val, 1);
+    EXPECT_EQ (l[0]->next->val, 4);
+    EXPECT_EQ (l[0]->next->next->val, 5);
+    EXPECT_EQ (l[1]->val, 1);
+    EXPECT_EQ (l[1]->next->val, 3);
+    EXPECT_EQ (l[1]->next->next->val, 4);
+    EXPECT_EQ (l[2]->val, 2);
+    EXPECT_EQ (l[2]->next->val, 6);
 }
 
 
